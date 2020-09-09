def7c86deb31e78d0f9c4000182e7d87fec2471b
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-09 14:38:28 +0100

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, Blueprint
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,4 +16,6 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     """ main function"""
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    app.run(host=getenv("HBNB_API_HOST"),
+            port=getenv("HBNB_API_PORT"),
+            threaded=True)
