8e0de17606f96439ad38ba9cb83ea4d87eb136ab
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-09 16:55:01 +0100

    errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,7 +19,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def not_found(error):
     """ handle errors"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response({'error': 'Not found'}, 404)
 
 if __name__ == "__main__":
     """ main function"""
