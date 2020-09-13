f4c3f74b3e182bae0ee3f4f29bc18df9e6a38cf0
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-13 16:13:53 +0100

    task8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -59,9 +59,9 @@ def post_city(state_id=None):
         if "name" not in city:
             abort(400, "Missing name")
         else:
-            city['state_id'] = state.id
+            city['state_id'] = state_id
             new_city = City(**city)
-            #storage.new(new_city)
+            storage.new(new_city)
             storage.save()
             return jsonify(new_city.to_dict()), 201
     abort(404)
