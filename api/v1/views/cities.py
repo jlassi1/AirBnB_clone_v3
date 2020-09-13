ac754c32dfb89cfed87fa7e739dd3ee706f6a23f
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-13 16:01:56 +0100

    task8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -17,19 +17,18 @@ from models.city import City
     strict_slashes=False)
 def states_cities(state_id=None, city_id=None):
     """State objects that handles all default RestFul API actions"""
-    if city_id is not None:
-        if storage.get(City, city_id) is not None:
+    if city_id:
+        if storage.get(City, city_id):
             return jsonify(storage.get(City, city_id).to_dict())
         else:
             abort(404)
-    if state_id is not None:
+    if state_id:
         state = storage.get(State, state_id)
         if not state:
             abort(404)
         list_cities = []
-        for cities in storage.all(City).values():
-            if cities.id == state_id:
-                list_cities.append(cities.to_dict())
+        for cities in state.cities:
+            list_cities.append(cities.to_dict())
         return jsonify(list_cities)
     else:
         abort(404)
@@ -51,14 +50,14 @@ def del_city(city_id=None):
 @app_views.route(
     '/states/<state_id>/cities', methods=['POST'],
     strict_slashes=False)
-def post_city(state_id):
+def post_city(state_id=None):
     """ post city """
     if not storage.get(State, state_id):
         abort(404)
     city = request.get_json()
-    if city is None:
+    if not city:
         abort(400, "Not a JSON")
-    elif "name" not in city.keys():
+    if "name" not in city.keys():
         abort(400, "Missing name")
     else:
         city['state_id'] = state.id
@@ -71,16 +70,14 @@ def post_city(state_id):
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id=None):
     """ put city """
-    updated = storage.get(City, city_id)
-    if updated is None:
-        abort(404)
-    city = request.get_json()
+    city = storage.get(City, city_id)
     if city is None:
+        abort(404)
+    updated = request.get_json()
+    if updated is None:
         abort(400, "Not a JSON")
-    for k, v in city.items():
-        if k in ['id', 'created_at', 'updated_at']:
-            pass
-        else:
-            setattr(updated, k, v)
+    for k, v in updated.items():
+        if k not in ['id', 'created_at', 'updated_at']:
+            setattr(city, k, v)
     storage.save()
     return jsonify(updated.to_dict()), 200
